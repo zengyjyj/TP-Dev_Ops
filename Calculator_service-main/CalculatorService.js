e3f91b4798074725f8d2383eb5339a0aa3699057
yuZhang <122043326+yzhan2911@users.noreply.github.com>
2024-11-22 08:31:09 +0100

    Update CalculatorService.js

--- Calculator_service-main/CalculatorService.js
+++ Calculator_service-main/CalculatorService.js
@@ -2,7 +2,7 @@ var http = require ('http');
 var request = require('sync-request');
 
 const PORT = 80;
-const service_ip = '192.168.37.27';
+const service_ip = '192.168.37.84';
 
 const SUM_SERVICE_IP_PORT = 'http://'+service_ip+':31001';
 const SUB_SERVICE_IP_PORT = 'http://'+service_ip+':31002';
